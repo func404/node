7ec147a98a818f4e2281d955668cbfb564cffdcb
Jingxinpo <492971588@qq.com>
2019-03-11 10:34:08 +0800

    提条koa

---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+* @Author: smile
+* @Date:   2019-02-27 14:44:34
+* @Last Modified by:   smile
+* @Last Modified time: 2019-03-11 09:38:09
+*/
+const config = {
+
+    port: 3001,
+
+    database: {
+        DATABASE: 'wlxs',
+        USERNAME: 'root',
+        PASSWORD: '220724',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
